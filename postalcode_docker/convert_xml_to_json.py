b2c1d6ec717fc9164f18ce917619e9a64d386c8b
pedro <osodrac_ordep@hotmail.com>
2022-08-08 21:05:52 -0500

    Update python script to convert xml source to json
    Add normalize function to remove acents and set names uppercase

--- postalcode_docker/convert_xml_to_json.py
+++ postalcode_docker/convert_xml_to_json.py
@@ -14,22 +14,22 @@ def create_json_data_from_xml(source_file='CPdescarga.xml'):
         if item['d_codigo'] not in clean_data.keys():
             clean_data[item['d_codigo']] = {
                 "zip_code": item['d_codigo'],
-                "locality": item.get('d_ciudad', item['D_mnpio']),
+                "locality": normalize(item.get('d_ciudad', item['D_mnpio'])),
                 "federal_entity": {
                     "key": int(item['c_estado']),
-                    "name": item['d_estado'],
+                    "name": normalize(item['d_estado']),
                     "code": None
                 },
                 "settlements": [],
                 "municipality": {
                     "key": int(item['c_mnpio']),
-                    "name": item['D_mnpio']
+                    "name": normalize(item['D_mnpio'])
                 }
             }
         clean_data[item['d_codigo']]['settlements'].append({
             "key": int(item['id_asenta_cpcons']),
-            "name": item['d_asenta'],
-            "zone_type": item['d_zona'],
+            "name": normalize(item['d_asenta']),
+            "zone_type": normalize(item['d_zona']),
             "settlement_type": {
                 "name": item['d_tipo_asenta']
             }
@@ -38,6 +38,19 @@ def create_json_data_from_xml(source_file='CPdescarga.xml'):
     print(" >>>>>> saving json to data.json")
     with open('data.json', 'w') as destination:
         destination.write(json.dumps(list(clean_data.values())))
+
+def normalize(string):
+    replacements = (
+        ("Á", "A"),
+        ("É", "E"),
+        ("Í", "I"),
+        ("Ó", "O"),
+        ("Ú", "U"),
+    )
+    for a, b in replacements:
+        string = string.replace(a, b).replace(a.lower(), b.lower())
+
+    return string.upper()
 
 if '__main__' == __name__:
     create_json_data_from_xml()
